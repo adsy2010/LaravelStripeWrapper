fd8d2fb3d238e8ba0c711f5312fac7a09317890f
adsy2010 <adsy2010@users.noreply.github.com>
2021-04-02 21:49:10 +0100

    Added migration publishing

--- src/LaravelStripeWrapperServiceProvider.php
+++ src/LaravelStripeWrapperServiceProvider.php
@@ -24,5 +24,9 @@ class LaravelStripeWrapperServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        $this->publishes([
+            __DIR__.'/database/migrations' => database_path('migrations')
+        ], 'migrations');
     }
 }
