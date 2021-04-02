f0256d6572bdc065df566bac3df3f4e0ab397804
adsy2010 <adsy2010@users.noreply.github.com>
2021-04-02 23:43:20 +0100

    Added soft deletes Added fillable and guarded values Changed create to store method Added new include scopes method to chain with store method

--- src/Models/StripeCredential.php
+++ src/Models/StripeCredential.php
@@ -2,22 +2,57 @@
 
 namespace Adsy2010\LaravelStripeWrapper\Models;
 
+use Adsy2010\LaravelStripeWrapper\Exceptions\StripeScopeRequiredException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class StripeCredential
+ * @package Adsy2010\LaravelStripeWrapper\Models
+ * @mixin Builder
+ */
 class StripeCredential extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['key', 'value'];
+    protected $guarded = ['id','value'];
 
 
-    public static function create(array $data)
+    /**
+     * @param array $data
+     * @param array $scopes
+     * @throws StripeScopeRequiredException
+     */
+    public function store(array $data)
     {
+
         $key = $data['key']; //friendly name for key
         $value = $data['value']; //encrypted here
 
         $encrypted = encrypt($value);
 
-        return parent::create(['key' => $key, 'value' => $encrypted]);
+        $this->key = $key;
+        $this->value = $encrypted;
+
+        $this->save();
+
+        return $this;
+    }
+
+    public function includeScopes($scopes = [StripeScope::PUBLISHABLE], $access = 'r')
+    {
+        if(count($scopes) === 0) {
+            throw new StripeScopeRequiredException();
+        }
+
+        foreach ($scopes as $scope) {
+            (new StripeCredentialScope)->create(['stripe_credentials_id' => $this->id, 'stripe_scope_id' => $scope, 'access' => $access]);
+        }
+
+        return $this;
     }
 
 
