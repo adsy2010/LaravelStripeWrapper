3a136eed8fd02830b8495c6bde762909b22b135c
adsy2010 <adsy2010@users.noreply.github.com>
2021-04-10 21:28:47 +0100

    Added tests for stripe products

--- tests/Unit/StripeProductTest.php
+++ tests/Unit/StripeProductTest.php
@@ -2,6 +2,9 @@
 
 namespace Adsy2010\LaravelStripeWrapper\Tests\Unit;
 
+use Adsy2010\LaravelStripeWrapper\Models\StripeCredential;
+use Adsy2010\LaravelStripeWrapper\Models\StripeProduct;
+use Adsy2010\LaravelStripeWrapper\Models\StripeScope;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Tests\TestCase;
@@ -13,17 +16,59 @@ class StripeProductTest extends TestCase
     /**
      * @test
      */
-    function the_stripe_product_is_created()
+    function the_stripe_product_is_created_on_stripe()
     {
-        $this->assertTrue(true);
+        $product = ['name' => 'My test product'];
+
+        $secretCredentials = (new StripeCredential)
+            ->store(['key' => 'secret', 'value' => env('STRIPE_SECRET_KEY')])
+            ->includeScopes([StripeScope::SECRET], 'w');
+
+        $createdProduct = (new StripeProduct)->store($product);
+
+        $this->assertArrayHasKey('id', $createdProduct);
+
+        $this->the_stripe_product_is_deleted($createdProduct->id);
+
+        $secretCredentials->delete();
     }
 
     /**
      * @test
      */
-    function the_stripe_product_exists()
+    function the_stripe_product_is_created_on_stripe_and_locally()
     {
-        $this->assertTrue(true);
+        $product = ['name' => 'My test product'];
+
+        $secretCredentials = (new StripeCredential)
+            ->store(['key' => 'secret', 'value' => env('STRIPE_SECRET_KEY')])
+            ->includeScopes([StripeScope::SECRET], 'w');
+
+        $createdProduct = (new StripeProduct)->store($product, true);
+
+        $this->the_stripe_product_exists($createdProduct);
+
+        $localStripeProduct = $this->the_stripe_product_exists_locally($createdProduct->id);
+
+        $this->the_stripe_product_is_deleted($createdProduct->id, true);
+
+        $this->assertSoftDeleted($localStripeProduct);
+
+        $secretCredentials->delete();
+    }
+
+    function the_stripe_product_exists($productId)
+    {
+        $this->assertArrayHasKey('id', $productId);
+    }
+
+    function the_stripe_product_exists_locally($stripeProductId)
+    {
+        $localStripeProduct = StripeProduct::find($stripeProductId);
+
+        $this->assertTrue($stripeProductId === $localStripeProduct->id);
+
+        return $localStripeProduct;
     }
 
     /**
@@ -42,11 +87,10 @@ class StripeProductTest extends TestCase
         $this->assertTrue(true);
     }
 
-    /**
-     * @test
-     */
-    function the_stripe_product_is_deleted()
+    function the_stripe_product_is_deleted($id, $store = false)
     {
-        $this->assertTrue(true);
+        $stripeProduct = (new StripeProduct)->trash($id, $store);
+
+        $this->assertTrue($stripeProduct->deleted);
     }
 }
