f244ad5a43979a2746843566b5ed5dd4f7aba8d4
adsy2010 <adsy2010@users.noreply.github.com>
2021-05-21 21:46:13 +0100

    Cast shipping as a bool Added shipping to fillable Added shipping relationship

--- src/Models/StripeCustomerAddress.php
+++ src/Models/StripeCustomerAddress.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
@@ -24,9 +25,9 @@ class StripeCustomerAddress extends Model
 
     protected $guarded = ['id'];
 
-    protected $fillable = ['stripe_customer_id', 'line1', 'line2', 'city', 'state', 'postal_code', 'country'];
+    protected $fillable = ['stripe_customer_id', 'line1', 'line2', 'city', 'state', 'postal_code', 'country', 'shipping'];
 
-    protected $casts = [];
+    protected $casts = ['shipping' => 'bool'];
 
     /**
      * The customer related to this customer address
@@ -36,5 +37,15 @@ class StripeCustomerAddress extends Model
     public function customer(): BelongsTo
     {
         return $this->belongsTo(StripeCustomer::class);
+    }
+
+    /**
+     * The shipping details related to this customer address
+     *
+     * @return HasOne
+     */
+    public function shipping(): HasOne
+    {
+        return $this->hasOne(StripeCustomerShippingDetails::class);
     }
 }
